67edfa7abaf1e535991caa186ab64e099fa4e164
Areejslama <areegahmed449@gmail.com>
2024-08-02 01:43:05 +0300

    sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""this script define a function"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """define the dunction"""
+    return sum(input_list)
