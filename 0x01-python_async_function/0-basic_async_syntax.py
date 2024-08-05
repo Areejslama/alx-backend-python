208e0a3f2365ca12baaf4e03061eb904f8d0b22a
Areejslama <areegahmed449@gmail.com>
2024-08-05 23:26:13 +0300

    0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""this script to define a function"""
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int) -> float:
+    """define the function"""
+    await asyncio.sleep(delay)
+
+    delay = random.uniform(max_delay)
+    return delay
+
+if __name__ == "__main__":
+    asyncio.run(wait_random(delay))
