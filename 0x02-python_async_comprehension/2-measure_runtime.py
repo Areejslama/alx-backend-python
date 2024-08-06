d002493fe55da002af9cf164eda474238e2c1216
Areejslama <areegahmed449@gmail.com>
2024-08-07 00:53:36 +0300

    2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,13 +2,14 @@
 """This module provides an asynchronous list comprehension"""
 
 import asyncio
-from random import uniform
-
+import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> float:
+async def measure_runtime():
     """define a function"""
-    for _ in range(4):
-        time = await asyncio.gather(async_comprehension())
-        return delay
+    start = time.time()
+    end = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    total = end - start
+    return total
