322601b09ee0e69122f06a99db11c238f63d801e
Areejslama <areegahmed449@gmail.com>
2024-08-07 01:09:03 +0300

    2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,8 +8,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """define a function"""
-    start_time = time()
-    total = [async_comprehension() for i in range(4)]
-    await gather(*total)
-    end_time = time()
-    return (end_time - start_time)
+    start = time()
+    tasks = [async_comprehension() for i in range(4)]
+    await gather(*tasks)
+    end = time()
+    return (end - start)
