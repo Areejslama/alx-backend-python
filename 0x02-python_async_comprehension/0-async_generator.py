2c160e634fcd1b57566a601942404ccef4615bd1
Areejslama <areegahmed449@gmail.com>
2024-08-07 02:02:59 +0300

    00

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """this script define a function"""
-import asyncio
+from asyncio import sleep
 from random import uniform
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
     """define the function"""
-    await asyncio.sleep(1)
-
     for _ in range(10):
+        await sleep(1)
 
         yield uniform(0, 10)
