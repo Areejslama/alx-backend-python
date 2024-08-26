d0920a9030b32653325b7c25ceab325eae238273
Areejslama <areegahmed449@gmail.com>
2024-08-26 23:47:28 +0300

    client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """this script to test methods"""
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
 from test_utils import TestGetJson
 from parameterized import parameterized
@@ -19,3 +19,15 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(value)
         client.org()
         mock.assert_called_once_with(f'https://api.github.com/orgs/{value}')
+
+    def test_public_repos_url(self):
+        """define function"""
+        with patch(
+                'client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock
+                ) as mock:
+            payload = "World"
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload)
