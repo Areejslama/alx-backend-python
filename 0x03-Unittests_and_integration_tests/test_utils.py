c244da78575f9833b6a9420dd87553043de92997
Areejslama <areegahmed449@gmail.com>
2024-08-26 15:16:55 +0300

    0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,32 +6,6 @@ from utils import (access_nested_map, get_json, memoize)
 from unittest.mock import patch, Mock
 
 
-class TestMemoize(unittest.TestCase):
-    """ Class for Testing Memoize """
-
-    def test_memoize(self):
-        """ Test that when calling a_property twice, the correct result
-        is returned but a_method is only called once using
-        assert_called_once
-        """
-
-        class TestClass:
-            """ Test Class for wrapping with memoize """
-
-            def a_method(self):
-                return 42
-
-            @memoize
-            def a_property(self):
-                return self.a_method()
-
-        with patch.object(TestClass, 'a_method') as Mock:
-            test_class = TestClass()
-            test_class.a_property()
-            test_class.a_property()
-            Mock.assert_called_once()
-
-
 class TestAccessNestedMap(unittest.TestCase):
     """define class"""
     @parameterized.expand([
@@ -67,3 +41,29 @@ class TestGetJson(unittest.TestCase):
         with patch('requests.get', return_value=Mock(**json)) as request:
             self.assertEqual(get_json(test_url), test_payload)
             request.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Class for Testing Memoize """
+
+    def test_memoize(self):
+        """ Test that when calling a_property twice, the correct result
+        is returned but a_method is only called once using
+        assert_called_once
+        """
+
+        class TestClass:
+            """ Test Class for wrapping with memoize """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock_method.assert_called_once()
